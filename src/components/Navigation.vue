c9f77c58f0e056106e291fde43eef9afb4af2318
Semen Alekseev <semyon492@ya.ru>
2023-09-02 13:28:26 +0300

    upd

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -56,7 +56,7 @@ export default {
             }
         })
         .then(res => {
-            console.log(res);
+            console.log(res.data);
             if(res.data.roles == 'ROLE_ADMIN'){
                 this.is_admin = true;
             } 
