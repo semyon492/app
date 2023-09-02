ae156b0bb6a4aac015feac0ab90f31944723e3b9
Semen Alekseev <semyon492@ya.ru>
2023-09-02 14:43:40 +0300

    upd

--- src/views/public/Home.vue
+++ src/views/public/Home.vue
@@ -2,7 +2,7 @@
   <main v-if="(is_connected !== true)">
     <section class="pt-5 pb-0 position-relative">
       <!-- Container START -->
-      <div class="container">
+      <div class="container-fluid">
         <!-- Row START -->
         <div class="row text-center position-relative z-index-1">
           <div class="col-lg-7 mx-auto">
@@ -42,50 +42,24 @@
                   <img src="https://bayirdan.github.io/facebook-clone/assets/images/pp.jpg" class="rounded-circle w-9" alt="">
                   <span class="fw-bold text-muted">{{ user.firstname }} {{ user.lastname }}</span>
                 </li>
-                <li class="flex items-center gap-2 px-3 py-2 w-full cursor-pointer rounded-lg hover:bg-myGray-150 transition-color duration-300">
+                <!-- <li class="flex items-center gap-2 px-3 py-2 w-full cursor-pointer rounded-lg hover:bg-myGray-150 transition-color duration-300">
                   <img src="https://bayirdan.github.io/facebook-clone/assets/images/png/friends.png" class="rounded-full w-9" alt=""><span
                     class="text-myGray-800 font-bold text-tiny">Friends</span>
-                </li>
-                <li class="flex items-center gap-2 px-3 py-2 w-full cursor-pointer rounded-lg hover:bg-myGray-150 transition-color duration-300">
-                  <img src="https://bayirdan.github.io/facebook-clone/assets/images/png/groups.png" class="rounded-full w-9" alt=""><span
-                    class="text-myGray-800 font-bold text-tiny">Groups</span>
-                </li>
-
-                <li class="flex items-center gap-2 px-3 py-2 w-full cursor-pointer rounded-lg hover:bg-myGray-150 transition-color duration-300">
-                  <img src="https://bayirdan.github.io/facebook-clone/assets/images/png/marketplace.png" class="rounded-full w-9" alt=""><span
-                    class="text-myGray-800 font-bold text-tiny">Marketplace</span></li>
-                <li class="flex items-center gap-2 px-3 py-2 w-full cursor-pointer rounded-lg hover:bg-myGray-150 transition-color duration-300">
-                  <img src="https://bayirdan.github.io/facebook-clone/assets/images/png/watch.png" class="rounded-full w-9" alt=""><span
-                    class="text-myGray-800 font-bold text-tiny">Watch</span></li>
-                <li class="flex items-center gap-2 px-3 py-2 w-full cursor-pointer rounded-lg hover:bg-myGray-150 transition-color duration-300">
-                  <img src="https://bayirdan.github.io/facebook-clone/assets/images/png/memories.png" class="rounded-full w-9" alt=""><span
-                    class="text-myGray-800 font-bold text-tiny">Memories</span></li>
-                <li class="flex items-center gap-2 px-3 py-2 w-full cursor-pointer rounded-lg hover:bg-myGray-150 transition-color duration-300">
-                  <img src="https://bayirdan.github.io/facebook-clone/assets/images/png/saved.png" class="rounded-full w-9" alt=""><span
-                    class="text-myGray-800 font-bold text-tiny">Saved</span></li>
-                <li class="flex items-center gap-2 px-3 py-2 w-full cursor-pointer rounded-lg hover:bg-myGray-150 transition-color duration-300">
-                  <img src="https://bayirdan.github.io/facebook-clone/assets/images/png/pages.png" class="rounded-full w-9" alt=""><span
-                    class="text-myGray-800 font-bold text-tiny">Pages</span></li>
-                <li class="flex items-center gap-2 px-3 py-2 w-full cursor-pointer rounded-lg hover:bg-myGray-150 transition-color duration-300">
-                  <img src="https://bayirdan.github.io/facebook-clone/assets/images/png/events.png" class="rounded-full w-9" alt=""><span
-                    class="text-myGray-800 font-bold text-tiny">Events</span></li>
-                <li class="flex items-center gap-2 px-3 py-2 w-full cursor-pointer rounded-lg hover:bg-myGray-150 transition-color duration-300">
-                  <img src="https://bayirdan.github.io/facebook-clone/assets/images/png/jobs.png" class="rounded-full w-9" alt=""><span
-                    class="text-myGray-800 font-bold text-tiny">Jobs</span></li>
+                </li> -->
                 <li class="flex items-center gap-2 px-3 py-2 w-full cursor-pointer rounded-lg hover:bg-myGray-150 transition-color duration-300">
                   <img src="https://bayirdan.github.io/facebook-clone/assets/images/png/mostRecent.png" class="rounded-full w-9" alt=""><span
-                    class="text-myGray-800 font-bold text-tiny">Most Recent</span></li>
+                    class="text-myGray-800 font-bold text-tiny">Настройки</span></li>
               </ul>
             </div>
             <div id="footer" class="w-full hidden 2xl:block">
               <div class="text-muted text-xs pl-1 mb-14">
-                <a class="hover:underline cursor-pointer">Privacy</a>·
-                <a class="hover:underline cursor-pointer">Terms</a>·
-                <a class="hover:underline cursor-pointer">Adversiting</a>·
-                <a class="hover:underline cursor-pointer">Ad Choices</a>·
-                <a class="hover:underline cursor-pointer">Cookies</a>·<br>
-                <a class="cursor-pointer">More</a>·
-                <a>Burkay © 2021</a>
+                <a router-link to="/privacy-and-terms" class="hover:underline cursor-pointer">Privacy</a>·
+                <a router-link to="/privacy-and-terms" class="hover:underline cursor-pointer">Terms</a>·
+                <a router-link to="/privacy-and-terms" class="hover:underline cursor-pointer">Adversiting</a>·
+                <a router-link to="/privacy-and-terms" class="hover:underline cursor-pointer">Ad Choices</a>·
+                <a router-link to="/privacy-and-terms" class="hover:underline cursor-pointer">Cookies</a>·<br>
+                <a router-link to="/privacy-and-terms" class="cursor-pointer">More</a>·
+                <a>Naame © 2023</a>
               </div>
             </div>
           </div>
@@ -99,49 +73,6 @@
           <div class="bg-myGray-100 h-screen px-5 py-5 hidden w-80 lg:block fixed">
             <div id="contacts" class="w-full flex items-center justfiy-between text-myGray-800"><span
                 class="flex-1 font-semibold text-base pl-5">Contacts</span>
-              <div id="contact-buttons" class="flex gap-1">
-                <div
-                    class="p-2 rounded-full hover:bg-myGray-200 cursor-pointer transition-colors duration-300">
-                  <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
-                       class="w-4">
-                    <g fill-rule="evenodd" transform="translate(-448 -544)">
-                      <path
-                          d="M457.25 552.5H455v2.25a.75.75 0 0 1-1.5 0v-2.25h-2.25a.75.75 0 0 1 0-1.5h2.25v-2.25a.75.75 0 0 1 1.5 0V551h2.25a.75.75 0 0 1 0 1.5m6.38-4.435a.62.62 0 0 0-.64.047l-2.49 1.634v-1.394a1.854 1.854 0 0 0-1.852-1.852l-8.796.002a1.854 1.854 0 0 0-1.851 1.852v6.793c0 1.021.83 1.852 1.852 1.852l1.147-.002h7.648a1.854 1.854 0 0 0 1.852-1.851v-1.392l2.457 1.61a.641.641 0 0 0 .673.071.663.663 0 0 0 .37-.601v-6.167c0-.26-.142-.49-.37-.602">
-                      </path>
-                    </g>
-                  </svg></div>
-                <div
-                    class="p-2 rounded-full hover:bg-myGray-200 cursor-pointer transition-colors duration-300">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                       class="w-4">
-                    <g fill-rule="evenodd" transform="translate(-448 -544)">
-                      <g fill-rule="nonzero">
-                        <path
-                            d="M10.743 2.257a6 6 0 1 1-8.485 8.486 6 6 0 0 1 8.485-8.486zm-1.06 1.06a4.5 4.5 0 1 0-6.365 6.364 4.5 4.5 0 0 0 6.364-6.363z"
-                            transform="translate(448 544)"></path>
-                        <path
-                            d="M10.39 8.75a2.94 2.94 0 0 0-.199.432c-.155.417-.23.849-.172 1.284.055.415.232.794.54 1.103a.75.75 0 0 0 1.112-1.004l-.051-.057a.39.39 0 0 1-.114-.24c-.021-.155.014-.356.09-.563.031-.081.06-.145.08-.182l.012-.022a.75.75 0 1 0-1.299-.752z"
-                            transform="translate(448 544)"></path>
-                        <path
-                            d="M9.557 11.659c.038-.018.09-.04.15-.064.207-.077.408-.112.562-.092.08.01.143.034.198.077l.041.036a.75.75 0 0 0 1.06-1.06 1.881 1.881 0 0 0-1.103-.54c-.435-.058-.867.018-1.284.175-.189.07-.336.143-.433.2a.75.75 0 0 0 .624 1.356l.066-.027.12-.061z"
-                            transform="translate(448 544)"></path>
-                        <path
-                            d="m13.463 15.142-.04-.044-3.574-4.192c-.599-.703.355-1.656 1.058-1.057l4.191 3.574.044.04c.058.059.122.137.182.24.249.425.249.96-.154 1.41l-.057.057c-.45.403-.986.403-1.411.154a1.182 1.182 0 0 1-.24-.182zm.617-.616.444-.444a.31.31 0 0 0-.063-.052c-.093-.055-.263-.055-.35.024l.208.232.207-.206.006.007-.22.257-.026-.024.033-.034.025.027-.257.22-.007-.007zm-.027-.415c-.078.088-.078.257-.023.35a.31.31 0 0 0 .051.063l.205-.204-.233-.209z"
-                            transform="translate(448 544)"></path>
-                      </g>
-                    </g>
-                  </svg></div>
-                <div
-                    class="p-2 rounded-full hover:bg-myGray-200 cursor-pointer transition-colors duration-300">
-                  <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                       class="w-4">
-                    <g fill-rule="evenodd" transform="translate(-446 -350)">
-                      <path
-                          d="M458 360a2 2 0 1 1-4 0 2 2 0 0 1 4 0m6 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0m-12 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0">
-                      </path>
-                    </g>
-                  </svg></div>
-              </div>
             </div>
             <div id="firends" class="border-b border-myGray-250">
               <ul>
