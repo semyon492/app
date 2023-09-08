4c1eecd687d7e06a0b6f385875aef053d04a6b8b
Semen Alekseev <semyon492@ya.ru>
2023-09-08 16:10:28 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,5 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
-import './assets/images/mostRecent.png'
-import './assets/images/pp.jpg'
-import './assets/images/friends.png'
-
-// import '@fortawesome/fontawesome-free/css/all.css'
-// import '@fortawesome/fontawesome-free/js/all.js'
 
 createApp(App).use(router).mount('#app')
