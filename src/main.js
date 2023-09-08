e01254a4cc60a7e0b91eaa000b76b86155842d07
Semen Alekseev <semyon492@ya.ru>
2023-09-08 21:03:05 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
 
 import i18n from "./plugins/i18n.js";
 
