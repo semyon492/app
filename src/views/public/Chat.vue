764c86eaa5652dbca81cdae03020d71d4b50b2f3
Semen Alekseev <semyon492@ya.ru>
2023-09-09 01:05:55 +0300

    upd

--- src/views/public/Chat.vue
+++ src/views/public/Chat.vue
@@ -1,34 +1,6 @@
 <template>
   <main v-if="(user.is_connected !== true)">
-    <section class="pt-5 pb-0 position-relative">
-      <!-- Container START -->
-      <div class="container-fluid">
-        <!-- Row START -->
-        <div class="row text-center position-relative z-index-1">
-          <div class="col-lg-7 mx-auto">
-            <!-- Heading -->
-            <h1 class="display-3">Лучшее социальное приложение</h1>
-            <p class="lead">Увидеть решительную доброту, счастье, застенчивую вежливость, домашнюю
-              обстановку, но понять, что смеющаяся шестерка сделала далеко.</p>
-            <div class="d-sm-flex justify-content-center">
-              <!-- button -->
-              <!-- <button class="btn btn-primary">Зарегистрируйтесь бесплатно</button> -->
-              <router-link to="/register" class="btn btn-primary">Зарегистрируйтесь
-                бесплатно</router-link>
-              <div class="mt-2 mt-sm-0 ms-sm-3">
-
-                <!-- Rating END -->
-                <i>"Я не могу поверить, что это бесплатно!"</i>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- Row END -->
-
-
-        <!-- Row END -->
-      </div>
-    </section>
+    <Main />
   </main>
   <main v-if="(user.is_connected)">
     <div class="container-fluid">
@@ -306,12 +278,14 @@
 import Axios from "axios";
 import config from "/config";
 import Menu from '@/components/Menu.vue'
+import Main from '@/components/Main.vue'
 
 export default {
   name: 'Home',
   props: ['user'],
   components: {
-    Menu
+    Menu,
+	Main
   }, 
   data() {
     return {
