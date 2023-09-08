1cc993ba1db279f268b70533b47cbc781b6dfc7c
Semen Alekseev <semyon492@ya.ru>
2023-09-08 16:06:43 +0300

    style

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './assets/images/mostRecent.png'
 import './assets/images/pp.jpg'
 import './assets/images/friends.png'
 
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
+// import '@fortawesome/fontawesome-free/css/all.css'
+// import '@fortawesome/fontawesome-free/js/all.js'
 
 createApp(App).use(router).mount('#app')
