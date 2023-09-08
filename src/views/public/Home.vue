03fd3aa4bc0c3b25d61783d0ecc761aed3fdec32
Semen Alekseev <semyon492@ya.ru>
2023-09-08 14:12:32 +0300

    upd

--- src/views/public/Home.vue
+++ src/views/public/Home.vue
@@ -506,100 +506,10 @@ export default {
   },  
   data() {
     return {
-<<<<<<< HEAD
-      nbr_article: 0,
-      all_articles: [],
-      selected_articles: [],
-      fav_articlesId: [],
-      fav_articlesFavId: [],
-      list_fav_articles: [],
-      search: '',
-
-      user_id: null,
-      is_connected: false,
-      is_admin:false,
-      show_fav: false,
-=======
->>>>>>> 52c6ae3159d8780c0c9893b70636722188405f81
       name: config.title,
     }
   },
   async mounted() {
-<<<<<<< HEAD
-    this.is_connected = false;
-    if (!!localStorage.getItem('token')){
-      this.is_connected = true;
-      this.access_token.access_token = localStorage.getItem('token');
-    }     
-    await Axios.post(config.domain + "account/getinfo", {
-                access_token: localStorage.getItem('token')
-        })
-        .then(res => {
-            console.log(res.data);
-            this.user.firstname = res.data.data.firstname;
-            this.user.lastname = res.data.data.lastname;
-            if(res.data.roles == 'ROLE_ADMIN'){
-                this.is_admin = true;
-            } 
-        })
-    // console.log("Token : " + (this.is_connected == true))
-    // return await this.init_articles().finally(
-    //     () => {
-    //         console.log("test : " + this.is_connected == false)
-    //         if(this.is_connected) {
-    //             console.log("gergerbgaerbgçbzeuiezugbdiycfvqsyuev")
-    //             this.getUserID().finally(
-    //             () => {
-    //                 console.log("trttt")
-    //                 this.get_fav_articles().finally( () => {
-    //                     let parent_btn = document.getElementById("pages_choice")
-    //                     let nbr_btn_page = Math.ceil(this.nbr_article/9)
-    //                     for(let i=0; (this.id < nbr_btn_page)?i<=nbr_btn_page:i<nbr_btn_page; i++){
-    //                         let btn = document.createElement("a");
-    //                         if(i == nbr_btn_page){
-    //                             btn.innerHTML = '<i class="fa fa-angle-right">';
-    //                                 btn.classList.add("next");
-    //                                 btn.href="/home/"+ (parseInt(this.id)+1);
-    //                             }else{
-    //                                 btn.innerHTML = i+1;
-    //                                 btn.href="/home/"+ (i+1);
-    //                             }
-    //                             btn.classList.add("page");
-    //                             parent_btn.appendChild(btn);
-    //                         }
-    //                         let list_pages_btn = document.getElementsByClassName("page");
-    //                     list_pages_btn[this.id - 1].classList.add("selected_page");
-    //                     document.getElementById("home_link").className = ["router-link-exact-active"];
-    //                     this.select_articles()
-    //                     this.sort_article()
-    //                 })
-    //             })
-    //         } else {
-    //             console.log("else")
-    //             let parent_btn = document.getElementById("pages_choice")
-    //             let nbr_btn_page = Math.ceil(this.nbr_article/9)
-    //             for(let i=0; (this.id < nbr_btn_page)?i<=nbr_btn_page:i<nbr_btn_page; i++){
-    //                 let btn = document.createElement("a");
-    //                 if(i == nbr_btn_page){
-    //                     btn.innerHTML = '<i class="fa fa-angle-right">';
-    //                         btn.classList.add("next");
-    //                         btn.href="/home/"+ (parseInt(this.id)+1);
-    //                     }else{
-    //                         btn.innerHTML = i+1;
-    //                         btn.href="/home/"+ (i+1);
-    //                     }
-    //                     btn.classList.add("page");
-    //                     parent_btn.appendChild(btn);
-    //                 }
-    //                 let list_pages_btn = document.getElementsByClassName("page");
-    //                 list_pages_btn[this.id - 1].classList.add("selected_page");
-    //                 document.getElementById("home_link").className = ["router-link-exact-active"];
-    //                 this.select_articles()
-    //                 this.sort_article()
-    //         }
-    // })
-=======
->>>>>>> 52c6ae3159d8780c0c9893b70636722188405f81
   },
   methods: {
     async getUserID() {
