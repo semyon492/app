29e3ddf29d1e9883840d59c567ffa1b15981e394
Semen Alekseev <semyon492@ya.ru>
2023-09-09 23:23:32 +0300

    upd

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export default {
     title: "Rapigram",
-    domain: "http://example.com/api",
+    domain: "https://api.rapigram.ru/api/",
     SEO_title: " - yunyuyuan blog",
     SEO_description: "云与原的博客，书写值得书写的东西", // description meta header
     SEO_keywords: "yunyuyuan,yunyuyuan's blog,云与原,云与原的博客", // keywords meta header
